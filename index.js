7f261fc0b3624bf00d9425e0512a4add46fcd61c
Matthew Tan <tanmatt804@gmail.com>
2021-10-05 15:05:44 +0000

    finished

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 //1
-function fizzBuzz(){
-  
-}
+// function fizzBuzz(){
+//   for(let i = 1; i <= 100; i++){
+//       if (i % 3 === 0 && i % 5 === 0) {
+//           console.log("FizzBuzz")
+//     }
+//         else if(i % 3 === 0) {
+//         console.log("Fizz")
+//     }
+//         else if(i % 5 === 0) {
+//           console.log("Buzz")
+//     } 
+//     else {
+//         console.log(i)
+//     }
+//   }
+// }
 
 //2
 function fizzBuzz(number){
-  
+  let answer = []
+  for(let i = 1; i <= number; i++) {
+      if(i % 3 === 0 && i % 5 === 0) {
+          answer.push("FizzBuzz");
+      }
+      else if (i % 3 === 0){
+          answer.push("Fizz")
+      }
+      else if (i % 5 === 0){
+          answer.push("Buzz")
+      }
+      else {
+          answer.push(i)
+      }
+  }
+  return answer;
 }
-
-
